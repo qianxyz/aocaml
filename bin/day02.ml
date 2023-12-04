11e42ac935be37bdb48e99f7136a75770c49900f
Qianxy <xyqian67@gmail.com>
2023-12-04 00:00:17 -0800

    Day 2 part 2

--- bin/day02.ml
+++ bin/day02.ml
@@ -19,11 +19,11 @@ let parse_line line =
            String.split_on_char ',' s
            |> List.map (fun s ->
                   let pair = String.trim s |> String.split_on_char ' ' in
-                  (List.nth pair 0 |> int_of_string, List.nth pair 1)))
+                  (List.nth pair 1, List.nth pair 0 |> int_of_string)))
   in
   (game_id, grabs)
 
-let possible (num, color) =
+let possible (color, num) =
   match color with
   | "red" -> num <= 12
   | "green" -> num <= 13
@@ -35,4 +35,20 @@ let () =
   |> List.filter (fun (_game_id, grabs) ->
          List.flatten grabs |> List.for_all possible)
   |> List.map (fun (game_id, _grabs) -> game_id)
+  |> List.fold_left ( + ) 0 |> Printf.printf "%d\n"
+
+let min_require grabs color =
+  let max a b = if a > b then a else b in
+  grabs
+  |> List.map (fun l -> List.assoc_opt color l |> Option.value ~default:0)
+  |> List.fold_left max 0
+
+let power grabs =
+  [ "red"; "green"; "blue" ]
+  |> List.map (min_require grabs)
+  |> List.fold_left Int.mul 1
+
+let () =
+  input |> String.trim |> String.split_on_char '\n' |> List.map parse_line
+  |> List.map (fun (_game_id, grabs) -> power grabs)
   |> List.fold_left ( + ) 0 |> Printf.printf "%d\n"
